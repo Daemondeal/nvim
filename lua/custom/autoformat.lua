6e1e233c6aa29fc68d2258e24ccf8d736d7b126e
Daemondeal <daemondaeal@gmail.com>
2025-08-06 21:07:06 +0200

    fix

--- lua/custom/autoformat.lua
+++ lua/custom/autoformat.lua
@@ -16,7 +16,7 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        local enabled_filetypes = { lua = true, javascript = true, typescript = true, python = false }
+        local enabled_filetypes = { lua = true, python = false }
         return {
           timeout_ms = 500,
           lsp_fallback = enabled_filetypes[vim.bo[bufnr].filetype],
@@ -24,12 +24,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { 'isort', 'black' },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        python = { 'isort', 'black' },
       },
     },
   },
